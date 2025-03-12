5601830723685a57252f004f6b99ee9eae0e25f2
MgKyaw <mgkyaw20112@gmail.com>
2025-03-12 22:21:48 +0700

    feat: Comment printing sum of numbers

--- Debug101/obj/Debug/net9.0/Debug101.AssemblyInfo.cs
+++ Debug101/obj/Debug/net9.0/Debug101.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Debug101")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3498d0aec631716e2e4da93d632273e432bde2b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b25b75cacf6f6ac0ebc853703034944b963f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Debug101")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Debug101")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
