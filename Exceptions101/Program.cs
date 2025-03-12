ea246ac775f45c69c590fd46933050e68279ce09
MgKyaw <mgkyaw20112@gmail.com>
2025-03-12 22:56:42 +0700

    feat: Add code for Unit 4

--- Exceptions101/Program.cs
+++ Exceptions101/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+Console.WriteLine(float1 / float2);
+Console.WriteLine(number1 / number2);
+Console.WriteLine("Exit program");
