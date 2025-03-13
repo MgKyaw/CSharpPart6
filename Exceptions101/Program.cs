18ec99d70d708ca9bc3e98f62db3b686bf47bff0
MgKyaw <mgkyaw20112@gmail.com>
2025-03-13 20:59:43 +0700

    feat: Add try/catch

--- Exceptions101/Program.cs
+++ Exceptions101/Program.cs
@@ -46,8 +46,15 @@ static void WriteMessage()
     int number2 = 0;
     byte smallNumber;
 
-    Console.WriteLine(float1 / float2);
-    Console.WriteLine(number1 / number2);
+    try
+    {
+        Console.WriteLine(float1 / float2);
+        Console.WriteLine(number1 / number2);
+    }
+    catch (DivideByZeroException ex)
+    {
+        Console.WriteLine($"Exception caught in WriteMessage: {ex.Message}");
+    }
     checked
     {
         smallNumber = (byte)number1;
