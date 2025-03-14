121c2a03366f0b2836f3998932fd55b8ea1b75a5
MgKyaw <mgkyaw20112@gmail.com>
2025-03-14 22:05:53 +0700

    feat: Comment challenge code

--- ThrowExceptions101/Program.cs
+++ ThrowExceptions101/Program.cs
@@ -68,70 +68,70 @@
 //     return average;
 // }
 
-string[][] userEnteredValues = new string[][]
-{
-            new string[] { "1", "2", "3"},
-            new string[] { "1", "two", "3"},
-            new string[] { "0", "1", "2"}
-};
+// string[][] userEnteredValues = new string[][]
+// {
+//             new string[] { "1", "2", "3"},
+//             new string[] { "1", "two", "3"},
+//             new string[] { "0", "1", "2"}
+// };
 
-try
-{
-    Workflow1(userEnteredValues);
+// try
+// {
+//     Workflow1(userEnteredValues);
 
-    Console.WriteLine("'Workflow1' completed successfully.");
-}
-catch(Exception ex)
-{
-    Console.WriteLine("An error occurred during 'Workflow1'.");
-    Console.WriteLine("Invalid data. User input values must be non-zero values.");
-}
+//     Console.WriteLine("'Workflow1' completed successfully.");
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine("An error occurred during 'Workflow1'.");
+//     Console.WriteLine("Invalid data. User input values must be non-zero values.");
+// }
 
-static void Workflow1(string[][] userEnteredValues)
-{
-    foreach (string[] userEntries in userEnteredValues)
-    {
-        try
-        {
-            Process1(userEntries);
+// static void Workflow1(string[][] userEnteredValues)
+// {
+//     foreach (string[] userEntries in userEnteredValues)
+//     {
+//         try
+//         {
+//             Process1(userEntries);
 
-            Console.WriteLine("'Process1' completed successfully.");
-            Console.WriteLine();
-        }
-        catch(FormatException ex)
-        {
-            Console.WriteLine("'Process1' encountered an issue, process aborted.");
-            Console.WriteLine("Invalid data. User input values must be valid integers.");
-            Console.WriteLine();
-        }
-    }
-}
+//             Console.WriteLine("'Process1' completed successfully.");
+//             Console.WriteLine();
+//         }
+//         catch(FormatException ex)
+//         {
+//             Console.WriteLine("'Process1' encountered an issue, process aborted.");
+//             Console.WriteLine("Invalid data. User input values must be valid integers.");
+//             Console.WriteLine();
+//         }
+//     }
+// }
 
-static void Process1(String[] userEntries)
-{
-    int valueEntered;
+// static void Process1(String[] userEntries)
+// {
+//     int valueEntered;
 
-    foreach (string userValue in userEntries)
-    {
-        bool integerFormat = int.TryParse(userValue, out valueEntered);
+//     foreach (string userValue in userEntries)
+//     {
+//         bool integerFormat = int.TryParse(userValue, out valueEntered);
 
-        if (integerFormat == true)
-        {
-            if (valueEntered != 0)
-            {
-                checked
-                {
-                    int calculatedValue = 4 / valueEntered;
-                }
-            }
-            else
-            {
-                throw new DivideByZeroException();
-            }
-        }
-        else
-        {
-            throw new FormatException();
-        }
-    }
-}
+//         if (integerFormat == true)
+//         {
+//             if (valueEntered != 0)
+//             {
+//                 checked
+//                 {
+//                     int calculatedValue = 4 / valueEntered;
+//                 }
+//             }
+//             else
+//             {
+//                 throw new DivideByZeroException();
+//             }
+//         }
+//         else
+//         {
+//             throw new FormatException();
+//         }
+//     }
+// }
